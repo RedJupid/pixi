87487dd754a188f7204425bb287d3309629263f0
zhuxianda <627393061@qq.com>
2019-07-30 15:11:26 +0800

    Initial commit

---
+++ src/main/java/com/xd/pixi/PixiApplication.java
@@ -0,0 +1,13 @@
+package com.xd.pixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xd/pixi/PixiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xd.pixi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PixiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
