d5f5186a63dcc8a354287668db423cc3bd2d4dc7
redjupid <627393061@qq.com>
2019-08-01 01:40:47 +0800

    加载资源

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,6 +1,6 @@
 (function(){
     let Game = window.Game = function(){
-
+        this.resUrl = "./res.json";
     }
 
     Game.prototype.init = function(){
@@ -12,36 +12,44 @@
         app.stage.addChild(land);
     }
 
-    Game.prototype.s
 
     Game.prototype.load = function () {
-        console.log(this);
-        PIXI.Loader.shared
-            .add([
-                "img/land.jpg",
-                "img/shadow.png",
-                "img/bomb1.png",
-                "img/role1_1_1.png"
-            ])
-            .on("progress", loadProgressHandler)
-            .load(setup);
+        this.res = {};
+        let self = this;
+        let alreadyDoneNumber = 0;
 
-        function loadProgressHandler(loader, resource) {
+        //发出请求，请求JSON文件
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4){
+                let resObj = JSON.parse(xhr.responseText);
+                //遍历数组
+                PIXI.Loader.shared
+                    .add(resObj.images)
+                    .on("progress", loadProgressHandler)
+                    .load(setup);
 
-            //Display the file `url` currently being loaded
-            console.log("loading: " + resource.url);
+                function loadProgressHandler(loader, resource) {
 
-            //Display the percentage of files currently loaded
-            console.log("progress: " + loader.progress + "%");
+                    //Display the file `url` currently being loaded
+                    console.log("loading: " + resource.url);
 
-            //If you gave your files names as the first argument
-            //of the `add` method, you can access them like this
-            //console.log("loading: " + resource.name);
+                    //Display the percentage of files currently loaded
+                    console.log("progress: " + loader.progress + "%");
+
+                    //If you gave your files names as the first argument
+                    //of the `add` method, you can access them like this
+                    //console.log("loading: " + resource.name);
+                }
+
+                function setup() {
+                    console.log("All files loaded");
+                }
+            }
         }
+        xhr.open("get",this.resUrl,true);
+        xhr.send(null);
 
-        function setup() {
-            console.log("All files loaded");
-        }
     }
 })();
 
